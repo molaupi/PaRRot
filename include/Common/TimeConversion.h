14f33bdced3c9a29197a48a740f3c16dfa499661
molaupi <molaupi@googlemail.com>
2026-09-21 13:35:13 +0200

    Conversion from KaRRi time to ULTRA time cannot truncate division.

--- include/Common/TimeConversion.h
+++ include/Common/TimeConversion.h
@@ -2,7 +2,7 @@
 
 namespace parrot {
     static constexpr int karriToULTRATime(int timeInOneTenthSeconds) {
-        return timeInOneTenthSeconds / 10;
+        return timeInOneTenthSeconds / 10 + (timeInOneTenthSeconds % 10 != 0);
     }
 
     static constexpr int ultraToKarriTime(int timeInSeconds) {
